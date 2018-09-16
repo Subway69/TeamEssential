da23aa193afb22116c571bc6ad7650fde43ae760
Reece <reecerox2@gmail.com>
2018-09-16 16:36:56 +1000

    Help page edits

--- help.php
+++ help.php
@@ -49,6 +49,11 @@ require_once "PHP/default.php";
                    
                    <?php
 
+<<<<<<< HEAD
+				   
+				   
+=======
+>>>>>>> master
 }
 ?>
 					</nav>
@@ -56,11 +61,23 @@ require_once "PHP/default.php";
 			</div>
 		</div>
 		<div id="ii4vcy" class="row c3690"><div id="iuxvnm" class="cell">
+<<<<<<< HEAD
+			<div class="c13731">Help
+			</div>
+				<p> For system administrators, please refer to the user & technical documentation *here*. </p>	
+				<p id="helptext"> Test text for testing </p>
+								
+			</div>
+		</div>
+		
+		
+=======
 			<div class="c13731">Help!
 			</div>
 				
 			</div>
 		</div>
+>>>>>>> master
 		<script>var items = document.querySelectorAll('#iitw8i');
 			for (var i = 0, len = items.length; i < len; i++) {
 			  (function(){var t,e=this,a="[data-tab]",n=document.body,r=n.matchesSelector||n.webkitMatchesSelector||n.mozMatchesSelector||n.msMatchesSelector,o=function(){var a=e.querySelectorAll("[data-tab-content]")||[];for(t=0;t<a.length;t++)a[t].style.display="none"},i=function(n){var r=e.querySelectorAll(a)||[];for(t=0;t<r.length;t++){var i=r[t],s=i.className.replace("tab-active","").trim();i.className=s}o(),n.className+=" tab-active";var l=n.getAttribute("href"),c=e.querySelector(l);c&&(c.style.display="")},s=e.querySelector(".tab-active"+a);s=s||e.querySelector(a),s&&i(s),e.addEventListener("click",function(t){var e=t.target;r.call(e,a)&&i(e)})}.bind(items[i]))();
