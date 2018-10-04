b89fded2552876e1e53e4cb0272571a23fe04431
Amikiri <frozen.hell08@gmail.com>
2018-10-04 22:58:07 +1000

    registration.php head fix
    removed head and nav. ported query header.php

--- registration.php
+++ registration.php
@@ -3,55 +3,14 @@
 <?php
 session_start();
 require_once "PHP/default.php";
+include_once('header.php');
 ?>
 <!-- need to add - logout functionality to menu -->
 <!-- need to add - graduate confirmation to register form -->
 
-<head>
-    <meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="stylesheet" href="CSS/master.css">
-    <link rel="stylesheet" href="CSS/style.css">
-	<link rel="stylesheet" href="CSS/class numbered.css">
-	<link rel="stylesheet" href="CSS/bootstrap.css">
-	<link rel="stylesheet" href="CSS/media.css">
-	<script src="JS/passwordMatch.js"></script>
-	<link rel="icon" href="img/favicon_feduni.ico">
-	<!--<script src="JS/bootstrap.min.js"></script>-->
-</head>
-
 <body>
     <title>Research Assistant Database</title>	
 		
-    <nav class="navbar"> 
-			
-		<div class="navbar-container">
-		
-            <a href="registration.html" class="navbar-brand"></a>
-			
-            <div id="i1pfjb" class="navbar-burger" >
-                <div class="navbar-burger-line"></div>
-                
-                <div class="navbar-burger-line"></div>
-                
-                <div class="navbar-burger-line"></div>
-               
-            </div> 
-			
-            <div data-gjs="navbar-items" class="navbar-items-c " >
-                <nav data-gjs="navbar-menu" class=" navbar-menu">
-				
-					<a href="home.php" class=" navbar-menu-link">Home</a>
-					<a href="help.php" class=" navbar-menu-link">Help</a>
-					<a href="profile.php"  class="navbar-menu-link ">Profile</a> <!-- data-highlightable="1" title="Profile"-->
-					<a href="account.php"  class="navbar-menu-link ">Account</a> <!--data-highlightable="1" title="Account" -->
-					<!-- add log out functionality to below -->
-					<a href="PHP/logout.php" data-highlightable="1" title="Account" class="navbar-menu-link gjs-comp-selected">Log Out</a>
-					
-				</nav>
-            </div>
-        </div>
-    </nav>
     <?php
     if(isset($_POST['sub1']))
     {   
