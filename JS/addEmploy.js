4c7a97cb9b7b896cd39c325535374de3c190a19d
Unknown <mmunro1995@gmail.com>
2018-10-18 20:05:55 +1100

    Fixed a small number bug

--- JS/addEmploy.js
+++ JS/addEmploy.js
@@ -100,7 +100,7 @@ document.getElementById('tasks1').addEventListener('keydown',function(ev)
 
 document.getElementById('managerPhone1').addEventListener('keydown', function(ev) {
     var key   = ev.keyCode;
-    if(!(key ==48||key ==49||key ==50||key ==51||key ==52||key ==53||key ==54||key ==55||key ==56||key ==57||key ==96||key ==97||key ==98||key ==99||key ==100||key ==101||key ==102||key ==103||key ==104||key ==105))
+    if(!(key ==8||key==46||key ==48||key ==49||key ==50||key ==51||key ==52||key ==53||key ==54||key ==55||key ==56||key ==57||key ==96||key ==97||key ==98||key ==99||key ==100||key ==101||key ==102||key ==103||key ==104||key ==105))
         {
             ev.preventDefault();
         }
